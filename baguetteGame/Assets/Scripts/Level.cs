0d26be1c3bf67b24e00821690ea5b75c33c8a58f
matta9001 <matta9001@gmail.com>
2015-06-12 20:27:46 -0500

    First Commit after making it

---
+++ baguetteGame/Assets/Scripts/Level.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Level : MonoBehaviour {
+
+	public int score;
+	public Text scoreBox;
+
+	void Start(){
+		/*This was the code to randomly spawn, i just plopped the global variables here
+		public GameObject butterParent;
+		public GameObject butter;
+		public int butterAmount;
+		public int radius;
+		for(int i = 0; i < butterAmount; i++){
+			GameObject butterClone;
+			butterClone = Instantiate(butter, new Vector3(Random.Range(-radius, radius),
+			 Random.Range(-radius, radius), Random.Range(-radius, radius)), Quaternion.identity) as GameObject;
+			butterClone.transform.parent = butterParent.transform;
+		}*/
+	}
+	public void addScore(){
+		score += 1;
+		scoreBox.text = ""+score;
+	}
+}
