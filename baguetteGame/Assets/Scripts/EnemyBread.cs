2f549c0980fdcd8ce85f908c8010a4d23a88565b
Matthew-PC <matta9001@gmail.com>
2015-06-21 00:32:18 -0500

    Explosions
    I made a particle system with explosions and added it to every piece of
    bread

--- baguetteGame/Assets/Scripts/EnemyBread.cs
+++ baguetteGame/Assets/Scripts/EnemyBread.cs
@@ -5,12 +5,14 @@ public class EnemyBread : MonoBehaviour {
 
 	public GameObject player;
 	public int speed;
+	public GameObject explosion;
 
 	void Update () {
 		transform.LookAt(player.transform, Vector3.up);
 		transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
 	}
 	void Hit(){
+		Instantiate(explosion, transform.position, Quaternion.identity);
 		GameObject.Destroy(gameObject);
 	}
 }
