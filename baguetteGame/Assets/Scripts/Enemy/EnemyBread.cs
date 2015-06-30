ea836a0c424e83fd95801b1a0b3dc1e17a2731fd
Matthew-PC <matta9001@gmail.com>
2015-06-30 01:33:58 -0500

    I started to work on the ai of the enemy bread so it goes around obstacles

--- baguetteGame/Assets/Scripts/Enemy/EnemyBread.cs
+++ baguetteGame/Assets/Scripts/Enemy/EnemyBread.cs
@@ -9,10 +9,27 @@ public class EnemyBread : MonoBehaviour {
 	public GameObject explosion;
 	public int damage;
 
+	private bool go = true;
+
 	void Update () {
 		if(playerObject){
 			transform.LookAt(player.transform, Vector3.up);
-			transform.position = Vector3.MoveTowards(transform.position, playerObject.transform.position, speed * Time.deltaTime);
+
+	//		Vector3 positionHorizontal = new Vector3(playerObject.transform.position.x, transform.position.y, playerObject.transform.position.z);
+			Vector3 direction = playerObject.transform.position - transform.position;
+			if(Physics.Raycast(transform.position, direction, 5)){
+				transform.Translate(Vector3.up * Time.deltaTime * speed, Space.World);
+				go = false;
+			}else if(Physics.Raycast(transform.position, Vector3.down, 1)){
+				//transform.Translate(Vector3.up * Time.deltaTime * speed * 5);
+				transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+				go = true;
+			}else{
+				go = true;
+			}
+			if(go){
+				transform.position = Vector3.MoveTowards(transform.position, playerObject.transform.position, speed * Time.deltaTime);
+			}
 		}
 	}
 	void Hit(){
