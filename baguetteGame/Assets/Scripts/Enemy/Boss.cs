886f56b35e8006703e28a27821aa45d8c32f712b
matta9001 <matta9001>
2015-07-01 13:02:50 -0500

    I made the spawning interval and i made the death animation more realistic

--- baguetteGame/Assets/Scripts/Enemy/Boss.cs
+++ baguetteGame/Assets/Scripts/Enemy/Boss.cs
@@ -10,6 +10,10 @@ public class Boss : MonoBehaviour {
 	private bool isDying;
 	public GameObject fire;
 	public GameObject explosion;
+	public GameObject impact;
+	public GameObject enemyBread;
+	public int spawnY;
+	public int spawnXZ;
 
 	public float totalHits;
 	private float damage;
@@ -22,16 +26,15 @@ public class Boss : MonoBehaviour {
 		totalStep = bossHealth.anchoredPosition.x * 2 / totalHits;
 		guiStepState = bossHealth.anchoredPosition.x;
 		damage = health / totalHits;
+		Invoke("SpawnEnemy", 10);
 
 	}
 	void Update (){
 
-		if(isDying == false){
+		if(!isDying){
 			if(playerObject){
 				transform.position = Vector3.MoveTowards(transform.position, playerObject.transform.position, Time.deltaTime * speed);
 			}
-		}else{
-			transform.Translate(Vector3.down * Time.deltaTime * 20, Space.World);
 		}
 	}
 
@@ -39,11 +42,13 @@ public class Boss : MonoBehaviour {
 		if(collision.transform.tag != "CanBeShot" && collision.transform.tag == "Player"){
 			player.SendMessage("changeHealth", 75, SendMessageOptions.DontRequireReceiver);
 		}
-		if(collision.transform.tag != "CanBeShot" && collision.transform.tag != "Player" && isDying){
-			Instantiate(explosion, transform.position, Quaternion.identity);
-				finish();
-		}
 
+	}
+	void OnCollisionEnter(Collision other){
+		if(other.transform.tag != "CanBeShot" && other.transform.tag != "Player" && isDying){
+			Instantiate(explosion, transform.position, Quaternion.identity);
+			finish();
+		}
 	}
 	public void Hit(){
 		if(health > 0){
@@ -55,9 +60,34 @@ public class Boss : MonoBehaviour {
 			DestroyBoss();
 		}
 	}
+	void SpawnEnemy(){
+		if(!isDying){
+			int spawns = Random.Range(0, 3);
+			for(int i = 0; i <= spawns; i++){
+				int x = Random.Range(0, spawnXZ);
+				int z = Random.Range(0, spawnXZ);
+				int abs = Random.Range(0, 2);
+				if(abs == 1){
+					x = -x;
+					z = -z;
+				}
+				Vector3 spawnPosition = new Vector3(transform.localPosition.x + x, transform.localPosition.y + 3, transform.localPosition.z + z);
+
+				GameObject enemyBreadClone = Instantiate(enemyBread, spawnPosition, Quaternion.identity) as GameObject;
+				enemyBreadClone.SetActive(true);
+				GameObject impactClone = Instantiate(impact, spawnPosition, Quaternion.identity) as GameObject;
+				GameObject.Destroy(impactClone, 1);
+			}
+
+			int invokeTime = Random.Range(5, 10);
+			Invoke("SpawnEnemy", invokeTime);
+		}
+	}
 	void DestroyBoss(){
 		isDying = true;
 		fire.SetActive(true);
+		gameObject.GetComponent<Rigidbody>().isKinematic = false;
+		gameObject.GetComponent<Rigidbody>().useGravity = true;
 	}
 	void finish(){
 		Object.Destroy(gameObject);
