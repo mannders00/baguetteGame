3033c8a171eb4ffd5d3055e416148e7a491309ea
Matthew-PC <matta9001@gmail.com>
2015-07-15 14:07:24 -0500

    I made it so that if the hub scene begins with an integer of 1-5, it will start a sequence where the planet explodes, however if it is at 0 it will start at the origin and do no sequence

--- baguetteGame/Assets/Scripts/Level/PlanetDestruction.cs
+++ baguetteGame/Assets/Scripts/Level/PlanetDestruction.cs
@@ -3,27 +3,44 @@ using System.Collections;
 
 public class PlanetDestruction : MonoBehaviour {
 
-	public GameObject planet;
-	public int planetInt;
-	public Vector3 explosionPosition;
+	private GameObject planet;
 	public GameObject explosion;
+	public Animator animator;
+	public Transform player;
+	public int planetInt;
 
-	void Start(){
-		planetInt = 1;
-		DestructionInit(planetInt);
+	private Vector3 explosionPosition;
+	private bool isMoving = false;
 
-	}
-	void DestructionInit(int planetIntInp){
-		planetInt = planetIntInp;
+	public void Start(){
+		planetInt = 0;
 		switch(planetInt){
-			case 1: planet = GameObject.Find("Planet1"); break;
-			case 2: planet = GameObject.Find("Planet2"); break;
-			case 3: planet = GameObject.Find("Planet3"); break;
-			case 4: planet = GameObject.Find("Planet4"); break;
-			case 5: planet = GameObject.Find("Planet5"); break;
+			case 0: break;
+			case 1: planet = GameObject.Find("Planet1"); player.transform.position = new Vector3(-1500, 250, -500); player.transform.rotation = Quaternion.Euler(0, 180, 0); break;
+			case 2: planet = GameObject.Find("Planet2"); player.transform.position = new Vector3(2469, -310, -59); player.transform.rotation = Quaternion.Euler(0, 0, 0); break;
+			case 3: planet = GameObject.Find("Planet3"); player.transform.position = new Vector3(2000, 316, -2774); player.transform.rotation = Quaternion.Euler(0, 90, 0); break;
+			case 4: planet = GameObject.Find("Planet4"); player.transform.position = new Vector3(-25, -291, -1676); player.transform.rotation = Quaternion.Euler(0, 90, 0); break;
+			case 5: planet = GameObject.Find("Planet5"); player.transform.position = new Vector3(-494, 14, 1621); player.transform.rotation = Quaternion.Euler(0, -90, 0); break;
 		}
-		planet.GetComponent<SphereCollider>().enabled = false;
-		explosionPosition = planet.transform.position;
+		if(planetInt == 1 || planetInt == 2 || planetInt == 3 || planetInt == 4 || planetInt == 5){
+			planet.GetComponent<SphereCollider>().enabled = false;
+			explosionPosition = planet.transform.position;
+			animator.SetTrigger("Explode");
+			isMoving = true;
+		}
 	}
-
+	public void Destruction(){
+		GameObject explosionInstantiate = Instantiate(explosion, explosionPosition, Quaternion.identity) as GameObject;
+		GameObject.Destroy(explosionInstantiate, 10);
+		GameObject.Destroy(planet, 0.5F);
+		planetInt = 0;
+	}
+	public void stopMoving(){
+		isMoving = false;
+	}
+	void Update(){
+		if(isMoving == true){
+			player.transform.Translate(-transform.right * Time.deltaTime * 300);
+		}
+	}
 }
--- baguetteGame/Assets/Scripts/Player/Character.cs
+++ baguetteGame/Assets/Scripts/Player/Character.cs
@@ -13,10 +13,6 @@ public class Character : MonoBehaviour {
 	public bool flying;
 	public GameObject thruster;
 	public Animator animator;
-//	public GameObject projectile;
-//	public GameObject projectileParent;
-//	private float cooldown = 0.5F;
-//	public float projectileForce;
 	public GameObject turboImage;
 	public Text header;
 	public GameObject rocket;
@@ -27,38 +23,34 @@ public class Character : MonoBehaviour {
 	private bool locked;
 	private int planetInt;
 	private bool cursorLockState = true;
-//	private int health = 100;
-//	public GameObject explosion;
-//	public Text healthText;
 
 	private Quaternion rocketRotation = Quaternion.identity;
 
 	private bool goingForward = false;
 	private bool goingBackward = false;
+	public GameObject level;
+	private bool thrusterBool;
 
 	void Start(){
-	//	cooldown = 0;
 		turbo = false;
 		turboImage.GetComponent<Image>().color = Color.green;
 		locked = false;
 		Cursor.lockState = CursorLockMode.Locked;
 		UnityEngine.Cursor.visible = false;
-//		InvokeRepeating("add1", 0, 1);
-//		healthText.text = "H " + health.ToString();
 	}
 	void Update(){
 
 		float x1;
 		if(turbo == true){
-			x1 = 50;
+			x1 = 58;
 		}else{
-			x1 = 10;
+			x1 = 18;
 		}
 		
 		//rocket.transform.localEulerAngles = Vector3.Lerp(rocket.transform.localEulerAngles, new Vector3(45, 0, 0), Time.deltaTime * 5);
 
 		Vector3 position1 = transform.TransformPoint(x1, 0, 0);
-		Vector3 position2 = transform.TransformPoint(3, 0, 0);
+		Vector3 position2 = transform.TransformPoint(13, 0, 0);
 
 		if(locked == false){
 			rocket.transform.localRotation = Quaternion.Lerp(rocket.transform.localRotation, rocketRotation, Time.deltaTime * 3);
@@ -73,8 +65,13 @@ public class Character : MonoBehaviour {
 				rb.AddRelativeForce(Vector3.right * flySpeed * Time.deltaTime * 10);
 			}
 			if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.W) || goingForward == true || goingBackward == true){
-				thruster.SetActive(true);
+				thrusterBool = true;
 			}else{
+				thrusterBool = false;
+			}
+			if(thrusterBool == true){
+				thruster.SetActive(true);
+				}else{
 				thruster.SetActive(false);
 			}
 
@@ -83,8 +80,8 @@ public class Character : MonoBehaviour {
 			Quaternion clamp = Quaternion.Euler(transform.localEulerAngles.x, transform.localEulerAngles.y, z);
 			transform.rotation = clamp;*/
 			
-	//		float horizontal = Input.GetAxis("Mouse X");
-			float horizontal = CrossPlatformInputManager.GetAxis("Horizontal") * sensitivity;
+			float horizontal = Input.GetAxis("Mouse X");
+	//		float horizontal = CrossPlatformInputManager.GetAxis("Horizontal") * sensitivity;
 			if(horizontal < 0){
 				if(z > 270 && z < 360 || z < 90){
 						transform.Rotate(Vector3.down * -horizontal * Time.deltaTime * rotateSpeed, Space.World);
@@ -99,8 +96,8 @@ public class Character : MonoBehaviour {
 						transform.Rotate(Vector3.down * horizontal * Time.deltaTime * rotateSpeed, Space.World);
 					}
 			}
-	//		float vertical = Input.GetAxis("Mouse Y");
-			float vertical = CrossPlatformInputManager.GetAxis("Vertical") * sensitivity;
+			float vertical = Input.GetAxis("Mouse Y");
+	//		float vertical = CrossPlatformInputManager.GetAxis("Vertical") * sensitivity;
 
 			if(vertical > 0){
 				transform.Rotate(Vector3.back * vertical * Time.deltaTime * rotateSpeed);
@@ -119,30 +116,8 @@ public class Character : MonoBehaviour {
 					UnityEngine.Cursor.visible = true;
 				}
 			}
-			}
 		}
-
-	/*
-	public void shoot(){
-		GameObject clone;
-		RaycastHit hit;
-
-		Vector3 raycastOrigin = cam.ViewportToWorldPoint(new Vector3(0.5F, 0.5F, 0));
-		Vector3 forward = transform.TransformDirection(Vector3.left) * 10;
-		Vector3 projectilePosition = transform.TransformPoint(-11.15F, -3.45F, -0.2F);
-
-		if(cooldown <= 0){
-			clone = Instantiate(projectile, projectilePosition, transform.rotation) as GameObject;
-			clone.transform.parent = projectileParent.transform;
-			cooldown = 0.5F;
-			if(Physics.Raycast(raycastOrigin, forward, out hit)){
-				if(hit.transform.tag == "CanBeShot"){
-					StartCoroutine(addScore(hit));
-				}
-			}
-		}
-	}*/
-
+	}
 	public void forwardTrue(){
 		goingForward = true;
 	}
@@ -155,27 +130,6 @@ public class Character : MonoBehaviour {
 	public void backwardFalse(){
 		goingBackward = false;
 	}
-	/*IEnumerator addScore(RaycastHit hit2){
-		yield return new WaitForSeconds(0.25F);
-		hit2.transform.SendMessage("Hit", SendMessageOptions.DontRequireReceiver);
-	}
-	public void Explode(){
-		locked = true;
-		rb.velocity =  Vector3.zero;
-		healthText.text = "H 0";
-	}
-	public void changeHealth(int healthInt){
-		if(health > 0){
-			health = health - healthInt;
-			healthText.text = "H " + health.ToString();
-		}
-	}
-	public void add1(){
-		if(health < 100 && health > 0){
-			health += 1;
-			healthText.text = "H " + health.ToString();
-		}
-	}*/
 	public void Turbo(){
 		turbo = !turbo;
 		if(turbo == true){
@@ -194,6 +148,23 @@ public class Character : MonoBehaviour {
 			case "Planet3": stop("White Bread (3)", 3); planet = 3; break;
 			case "Planet4": stop("Sourdough (4)", 4); planet = 4; break;
 			case "Planet5": stop("Rye (5)", 5); planet = 5; break;
+		}
+	}
+	public void PlanetDestruction(){
+		PlanetDestruction script = level.GetComponent<PlanetDestruction>();
+		script.Destruction();
+	}
+	public void stopMoving(){
+		PlanetDestruction script = level.GetComponent<PlanetDestruction>();
+		script.stopMoving();		
+	}
+	public void Lock(int lockState){
+		if(lockState == 1){
+			locked = true;
+			thrusterBool = true;
+		}else{
+			locked = false;
+			thrusterBool = false;
 		}
 	}
 	void stop(string planetText, int planetNumber){
